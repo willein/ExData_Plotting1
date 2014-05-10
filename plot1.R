1e0749f7322b3dbee1993494f2143c4fd7a89432
willein <marenich.mve@gmail.com>
2014-05-10 02:37:32 -0700

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -5,14 +5,11 @@ power <- read.table("household_power_consumption/household_power_consumption.txt
 #Change file
 
 datetime <- paste(power$Date, power$Time)
-datetime <- as.Date(strptime(datetime, "%d/%m/%Y %H:%M:%S"))
+datetime <-as.POSIXct(strptime(datetime, "%d/%m/%Y %H:%M:%S"))
 power <- cbind(datetime, power[,-(1:2)])
-
-
-#Subset and change
-
-power <- subset(power, datetime >= as.Date("2007-02-01") & datetime <= as.Date("2007-02-02"))
+power <- subset(power, datetime >= as.POSIXct("2007-02-01 00:00:00 PST") & datetime <= as.POSIXct("2007-02-02 23:59:59 PST"))
 power$Global_active_power = as.numeric(as.character(power$Global_active_power))
+
 
 #Create graph
 
